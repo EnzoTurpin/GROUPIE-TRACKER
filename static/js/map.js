618151df73bb9c73340d40460542a39aabd27ea8
Enzo Turpin <enzoturpin35@orange.fr>
2024-04-09 12:51:14 +0200

    Ajout de la possiblité de cliquer sur les locations

--- static/js/map.js
+++ static/js/map.js
@@ -6,13 +6,17 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
   var geocoder = new google.maps.Geocoder();
 
-  artistLocations.forEach(function (locationName) {
-    geocoder.geocode({ address: locationName }, function (results, status) {
+  var markers = {}; // Un objet pour stocker les marqueurs avec une clé unique pour chaque location
+
+  artistLocations.forEach(function (location, index) {
+    geocoder.geocode({ address: location }, function (results, status) {
       if (status === "OK") {
         var marker = new google.maps.Marker({
           map: map,
           position: results[0].geometry.location,
         });
+        // Utilisez l'index comme clé unique pour le marqueur
+        markers[index] = marker;
       } else {
         console.error(
           "Geocode was not successful for the following reason: " + status
@@ -20,4 +24,13 @@ function initMap() {
       }
     });
   });
+
+  // Ajouter ici la fonction centerMapOnMarker
+  window.centerMapOnMarker = function (markerKey) {
+    var marker = markers[markerKey];
+    if (marker) {
+      map.setCenter(marker.getPosition());
+      map.setZoom(15); // Ajustez le zoom selon vos besoins
+    }
+  };
 }
